d36cfd0ae9f36c234db6fa5bb68ee3a01954daa9
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-02-18 10:59:45 +0300

    feat: подключиться к Mongo

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+/* eslint-disable import/no-extraneous-dependencies */
 const express = require('express');
-
-const { PORT = 3000 } = process.env;
+const mongoose = require('mongoose');
 
 const app = express();
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+const { PORT = 3000 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
