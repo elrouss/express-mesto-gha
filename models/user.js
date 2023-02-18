031b41d4e9a1537eaa7371a863b38276b328b672
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-02-18 11:21:25 +0300

    feat: создать модель и схему пользователя (user)

---
+++ models/user.js
@@ -0,0 +1,37 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+    validate: {
+      validator(v) {
+        return v >= 2 && v <= 30;
+      },
+      message: 'Имя пользователя должно быть длиной от 2 до 30 символов',
+    },
+  },
+
+  about: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+    validate: {
+      validator(v) {
+        return v >= 2 && v <= 30;
+      },
+      message: 'Информация о пользователе должна быть длиной от 2 до 30 символов',
+    },
+  },
+
+  avatar: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
