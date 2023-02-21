a883770c4dc1f9102241195bbe85a4c63f7789c8
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-02-21 17:50:00 +0300

    fix: добавить ошибку в случае перехода по неправильному пути

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const routeCards = require('./routes/cards');
 const URL = 'mongodb://localhost:27017/mestodb';
 const { PORT = 3000 } = process.env;
 
+const { ERROR_NOT_FOUND } = require('./errors/errors');
+
 mongoose.set('strictQuery', true);
 mongoose.connect(URL);
 
@@ -26,5 +28,9 @@ app.use((req, res, next) => {
 });
 app.use('/users', routeUsers);
 app.use('/cards', routeCards);
+
+app.use((req, res, next) => {
+  next(res.status(ERROR_NOT_FOUND).send({ message: 'Такой страницы не существует' }));
+});
 
 app.listen(PORT);
