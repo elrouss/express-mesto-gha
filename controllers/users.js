835131d87981c97b36691bc37e76355878bdc3ac
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-02-23 11:47:59 +0300

    fix: исправить номер и текст ошибки при передаче некорректного id в случае обновления данных пользователя (404 -> 400)

--- controllers/users.js
+++ controllers/users.js
@@ -65,12 +65,8 @@ function setUserInfo(req, res) {
       return res.status(ERROR_NOT_FOUND).send({ message: 'Пользователь по указанному id не найден' });
     })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
         return res.status(ERROR_INACCURATE_DATA).send({ message: 'Переданы некорректные данные при обновлении профиля' });
-      }
-
-      if (err.name === 'CastError') {
-        return res.status(ERROR_NOT_FOUND).send({ message: 'Пользователь с указанным id не найден' });
       }
 
       return res.status(ERROR_INTERNAL_SERVER).send({ message: 'На сервере произошла ошибка' });
@@ -98,12 +94,8 @@ function setUserAvatar(req, res) {
       return res.status(ERROR_NOT_FOUND).send({ message: 'Пользователь по указанному id не найден' });
     })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
         return res.status(ERROR_INACCURATE_DATA).send({ message: 'Переданы некорректные данные при обновлении аватара' });
-      }
-
-      if (err.name === 'CastError') {
-        return res.status(ERROR_NOT_FOUND).send({ message: 'Пользователь с указанным id не найден' });
       }
 
       return res.status(ERROR_INTERNAL_SERVER).send({ message: 'На сервере произошла ошибка' });
